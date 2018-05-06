9a8c3786982da27c55b10e2e13204050293c575d
fjcamillo <fjcamillo.dev@gmail.com>
2018-05-06 13:25:09 +0800

    updated sstring

--- client.py
+++ client.py
@@ -7,12 +7,12 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe('wiznet/test')
 
 def on_message(client, userdata, msgs):
-    print(msgs.topic + str(msgs.payload))
+    print(msgs.topic +' '+ str(msgs.payload))
 
-    if msgs.payload == 'Hello':
+    if str(msgs.payload, 'utf8') == 'Hello':
         print('Received message #1. Do Something')
 
-    if msgs.payload == 'World':
+    if str(msgs.payload, 'utf8') == 'World':
         print('Received message #2: Do Something Else')
 
 
@@ -22,4 +22,4 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect('test.mosquitto.org', 1883, 60)
-client.loop_forever
+client.loop_forever()
